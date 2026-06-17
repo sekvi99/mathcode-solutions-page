d64882a654e7f427050a9cb02d41aa90fc3cd55e
Filip Kozlik <filmatkozlik@gmail.com>
2026-06-17 11:42:24 +0200

    Prepared prod application build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 export default defineConfig({
-  plugins: [tanstackStart(), react(), tailwindcss(), tsconfigPaths()],
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsconfigPaths(),
+    tanstackStart(),
+    nitro(),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
